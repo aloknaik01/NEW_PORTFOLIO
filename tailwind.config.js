bf805a81a72b8fdb70c55bc1f0c146dcfbb311a8
Alok Kumar Naik <aloknaik016261@gmail.com>
2025-12-29 22:26:05 +0530

    setup tailwind css with base configuration

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+     "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+       colors: {
+        'neon-lime': '#D9FF00',
+        'deep-space': '#020617',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+        handwriting: ['Caveat', 'cursive'],
+      },
+      animation: {
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+  plugins: [],
+}
